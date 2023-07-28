6cd741f8521471c24e96af4a5f989295a904b929
skennell <skennell@in-situ.com>
2023-07-28 10:08:24 -0600

    Adding some cards for Inserting Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export interface Card {
+  title: string;
+  image: string;
+  body: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'SoyProject';
+  public title = 'Soy\'s Project';
+  public cards:Card[] = [
+    {title:'Routes', image: 'Routes Image', body:"Welcome to Routes"},
+    {title:'Images', image: 'Generic Image', body:"Welcome to Images"},
+    {title:'Code', image: 'Code Image', body:"Welcome to Code"},
+    {title:'Author', image: 'Me Image', body:"About the Author"}
+  ];
+
 }
